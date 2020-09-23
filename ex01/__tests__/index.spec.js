d8be8e0008d740294a072e6ceebadcebaf646fc9
annie <wanganni@zhilunkeji.com>
2020-09-23 14:06:07 +0800

    feat: compose

--- ex01/__tests__/index.spec.js
+++ ex01/__tests__/index.spec.js
@@ -1,8 +1,7 @@
 test('练习01 自动化生成路由配置',() => {
     const {getRouter} = require('../index')
     const ret = getRouter(__dirname + '/data')
-    expect(getRouter(__dirname + '/data')).toBe(
-`
+    expect(getRouter(__dirname + '/data')).toBe(`
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
--- ex02/index.js
+++ ex02/index.js
@@ -1,18 +1,24 @@
 module.exports.compose = middlewares => {
     return function () {
-        return dispatch(0)
-        function dispatch(i) {
-            // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdPgd9gdwgywgddgdvgywgcRgywgdDgdngdcgdcgdPgd9gRygdggdmgd9gdeg9Ygdng9m
-gywgywgywgywgywgywgywgywgywgywgywgywgdngddgywgqRgy=gddgdvgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywg99gdmgdUgdDgdngdegd9gqDgdmgd9gdegdUgdPgRggd9gqRgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgRk
-gywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywg99gdmgdUgdDgdngdegd9gqDgdmgd9gdegdUgdPgRggd9gqR
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgddgdvgqRgddgd=gdvgdqgdwgdngdUgdvgywgdvgd9gRqgdwgqRgqkgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwgd=gdmgdvgywgdcgdngdegdYgdggdwgdqgdkgqRgdngywgqLgywgqYgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgRkgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgqk
-            // ##END##
+        const dispatch = (i) => {
+            let fn = middlewares[i]
+            if(!fn) {
+                return Promise.resolve()
+            }
+            return Promise.resolve(
+                fn(() => {
+                    return dispatch(i + 1)
+                })
+            )
         }
+
+        return dispatch(0);
     }
 }
+
+
+
+
+
+
+

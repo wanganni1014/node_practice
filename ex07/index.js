7b3523552fb4be52eaa6eecd4ce31114ae70b55e
annie <wanganni@zhilunkeji.com>
2020-10-19 08:07:24 +0800

    feat: 装饰器模式

--- ex07/index.js
+++ ex07/index.js
@@ -10,14 +10,12 @@ module.exports.createLoader = config => {
     }
 
     return {
+        // !暗号: 分治算法
         initFunction: scanFolder => {
             const ret = {}
-            // ##BEGIN## 代码已加密
-gywgywgywgywgywgywgywgywgywgywgywgywgdPgdUgdggdcgd9gdmgqRgdegdqgdggdvgcYgdUgdPgdcgd9gdmgqlgywgqRgddgdngdPgd9gdvgdggdDgd9gqlgywgdqgdmgd9gdggdwgd9gcYgd=gdvgqkgywgcRgckgywgRd
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdqgdUgdvgdegdwgywgddgd=gdvgdqgywgcRgywgdqgdmgd9gdggdwgd9gcYgd=gdvgqRgdqgdUgdvgddgdngdRgqk
-gywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgywgdmgd9gdwg9YgddgdngdPgd9gdvgdggdDgd9g9mgywgcRgywgddgd=gdvgdq
-gywgywgywgywgywgywgywgywgywgywgywgywgRkgqk
-            // ##END##
+            loader(scanFolder, (filename, file) => {
+                ret[filename] = file(config)
+            });
             return ret
         }
     }
